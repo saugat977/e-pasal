1a4d230c60acd443bc3a6e652f743a450335baf4
saugat977 <jungsaugat@gmail.com>
2022-08-23 07:20:55 +0545

    commiting practice

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
+const authRoutes = require('./routes/auth_route')
+
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.status(200).send('<center><h1>Welcome to Our Very Own Node App</h1></br> <h1>Happy Coding!</h1></center>')
 })
+
+app.use('/api/v1',authRoutes);
 
 app.listen(8000,()=>{
     console.log('server started on port 8000...');
